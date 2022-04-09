38420a599f599475c16a3139626e8f4c690a6560
abdulbositdev <abdulbosit.dev@gmail.com>
2022-04-09 15:21:18 +0500

    Initial commit

---
+++ js/min.js
@@ -0,0 +1,8 @@
+let themeSwitch = document.querySelector('.themeSwitch');
+let body = document.querySelector('body');
+
+themeSwitch.onclick = function(){
+    themeSwitch.classList.toggle('active');
+    body.classList.toggle('dark');
+};
+
